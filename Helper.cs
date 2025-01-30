a6c6a3af4796a8d5a07baf0f740fc8aa3b79e758
ByakuyT <beidengbaiye@gmail.com>
2025-01-30 16:43:22 +0800

    Update Helper.cs

--- Helper.cs
+++ Helper.cs
@@ -1,5 +1,3 @@
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 using HarmonyLib;
 using PotionCraft.Assemblies.DataBaseSystem.PreparedObjects;
@@ -9,7 +7,6 @@ using PotionCraft.ManagersSystem;
 using PotionCraft.ManagersSystem.Ingredient;
 using PotionCraft.ManagersSystem.Potion.Entities;
 using PotionCraft.ManagersSystem.TMP;
-using PotionCraft.ObjectBased.RecipeMap.RecipeMapItem.PotionEffectMapItem;
 using PotionCraft.ObjectBased.UIElements.Books.RecipeBook;
 using PotionCraft.ObjectBased.UIElements.FloatingText;
 using PotionCraft.ScriptableObjects;
@@ -30,31 +27,46 @@ namespace AlchAss
             RegisterLoc("dialog_deviation_status", "Deviation Status", "偏离信息");
             RegisterLoc("dialog_vortex_status", "Vortex Status", "漩涡信息");
             RegisterLoc("dialog_health_status", "Health Status", "血量信息");
+
             RegisterLoc("grind_progress", "PR: ", "进度: ");
+
             RegisterLoc("stir_stage", "ST: ", "阶段: ");
             RegisterLoc("stir_progress", "PR: ", "进度: ");
             RegisterLoc("stir_direction", "DI: ", "方向: ");
             RegisterLoc("stir_swamp", "ZO: ", "区域: ");
+
             RegisterLoc("closest_potion_path", "PO:", "目标: ");
             RegisterLoc("closest_general_path", "GE:", "总体: ");
             RegisterLoc("closest_distance_path", "DI:", "距离: ");
             RegisterLoc("closest_angle_path", "AN:", "旋转: ");
+
             RegisterLoc("closest_potion_ladle", "PO:", "目标: ");
             RegisterLoc("closest_general_ladle", "GE:", "总体: ");
             RegisterLoc("closest_distance_ladle", "DI:", "距离: ");
             RegisterLoc("closest_angle_ladle", "AN:", "旋转: ");
+
             RegisterLoc("position_distance", "DI: ", "距离: ");
             RegisterLoc("position_rotation", "RO: ", "旋转: ");
             RegisterLoc("position_angle", "AN: ", "方向: ");
             RegisterLoc("position_salt", "SA: ", "盐量: ");
+
             RegisterLoc("deviation_general", "GE: ", "总体: ");
             RegisterLoc("deviation_distance", "DI: ", "距离: ");
             RegisterLoc("deviation_rotation", "RO: ", "旋转: ");
+
             RegisterLoc("vortex_direction", "DI: ", "方向: ");
             RegisterLoc("vortex_angle", "AN: ", "夹角: ");
             RegisterLoc("vortex_dist", "DI: ", "距离: ");
             RegisterLoc("vortex_edge", "ED: ", "边缘: ");
+
             RegisterLoc("health_status", "HP: ", "血量: ");
+
+            RegisterLoc("aopen", "is opened", "已开启");
+            RegisterLoc("aclose", "is closed", "已关闭");
+
+            RegisterLoc("aline", "Indicator line", "指示线");
+            RegisterLoc("avortex", "Vortex approaching", "漩涡贴边");
+            RegisterLoc("aend", "End distance mode", "终点距离模式");
         }
         public static void SpawnMessageText(string msg)
         {
@@ -82,12 +94,6 @@ namespace AlchAss
             window.transform.SetParent(AlchAss.lab.transform, false);
             window.transform.localPosition = position;
             return window;
-        }
-        public static List<PotionEffectMapItem> GetNearestEffects()
-        {
-            Vector2 indicatorLocalPosition = Managers.RecipeMap.recipeMapObject.indicatorContainer.localPosition;
-            var availableEffects = Managers.RecipeMap.currentMap.referencesContainer.potionEffectsOnMap.Where(effect => effect.Status != PotionEffectStatus.Collected);
-            return availableEffects.OrderBy(effect => ((Vector2)effect.thisTransform.localPosition - indicatorLocalPosition).sqrMagnitude).ToList();
         }
         public static Vector2 StringToVector2(string input)
         {
